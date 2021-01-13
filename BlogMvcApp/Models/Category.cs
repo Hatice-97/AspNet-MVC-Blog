843f980f8c160461d17b63668749f7c4955b2e01
Hatice-97 <58397980+Hatice-97@users.noreply.github.com>
2021-01-13 15:59:31 +0300

    Create Category.cs

---
+++ BlogMvcApp/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BlogMvcApp.Models
+{
+    public class Category //Hem sınıfın kulllandığı model tablosu hem entity sınıfı
+    {
+        public int Id { get; set; }
+        public string KategoriAdi { get; set; }
+
+        public List<Blog> Bloglar { get; set; }
+    }
+}
